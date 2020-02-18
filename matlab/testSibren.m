371e0b24cfbcd80d6398186be7424e460711013a
Sibrenlagauw <sibren@mlagauw.com>
2020-02-18 15:47:08 +0100

    Finished linear implementation
    Model has still to be changed to linear pde instead of nonlinear pde...

--- matlab/testSibren.m
+++ matlab/testSibren.m
@@ -64,6 +64,8 @@ nodes = mesh.Nodes;
 nb_nodes = length(nodes);
 nb_elements_total = length(mesh.Elements(1,:));
 %% Iterate over all test functions
+A = zeros(2*nb_nodes, 2*nb_nodes);
+b = zeros(2*nb_nodes, 1);
 for testfunction_i = 1:nb_nodes
      % Get the node where the testfunction takes value 1 (zero in all other
      % nodes).
@@ -88,9 +90,9 @@ for testfunction_i = 1:nb_nodes
          element = nonzero_elements(:,j);
          index_test_function = find(element == testfunction_i);
          
-         node1_index = element(index_test_function); % Should equal testfunction_i
-         node2_index = element(mod(index_test_function+1,3));
-         node3_index = element(mod(index_test_function+2,3));
+         node1 = element(index_test_function); % Should equal testfunction_i
+         node2 = element(mod(index_test_function+1,3));
+         node3 = element(mod(index_test_function+2,3));
          X1 = nodes(node1); % (x1,y1) = (r1,z1)
          X2 = nodes(node2);
          X3 = nodes(node3);
@@ -119,24 +121,43 @@ for testfunction_i = 1:nb_nodes
          
          % SECOND INTEGRAL of (5) ----------------------------------------
          E = r(0,0,X1,X2,X3)*det_jac*V_mu;
-         % TODO: die nonlineaire term...
          
+         % NONLINEAR:
+         %nonlinear_term = E*c(node1)/(K_mu+K_mu/K_mv*c(node1+nb_nodes)+c(node1)+c(node1)*c(node1+nb_nodes)/K_mv);
          
+         % LINEAR:
+         int_5_2_lin = -V_mu*det_jac*r(0,0,X1,X2,X3)/4;
          
          % FIRST INTEGRAL of (6) ----------------------------------------
          c1_hat = D*det_jac*(sigma_vr+sigma_vz);
          c2_hat = -D*det_jac*sigma_vr;
          c3_hat = -D*det_jac*sigma_vz;
          
-         % SECOND INTEGRAL of (5) ----------------------------------------
-         % TODO: wiskundig uitwerken en die nonlineaire term...
+         % SECOND INTEGRAL of (6) ----------------------------------------
+         % NONLINEAR:
+         %nonlinear_term = ...
+         
+         % LINEAR:
+         int_6_2_lin = -r_q*V_mu*det_jac*r(0,0,X1,X2,X3)/4;
          
          % Alle termen toevoegen aan sparse system of equations
+         % TODO: termen van lijnintegraal Sem nog toevoegen...
+         % Ik denk dat het += is omdat we integraal van 1 testfunctie
+         % hebben opgesplitst in meerdere bijdrages over verschillende
+         % elementen. 
+         A(testfunction_i,node1) = A(testfunction_i,node1) + c1;
+         A(testfunction_i,node2) = A(testfunction_i,node2) + c2;
+         A(testfunction_i,node3) = A(testfunction_i,node3) + c3;
+         b(testfunction_i) = b(testfunction_i) + int_5_2_lin;
+         A(nb_nodes+testfunction_i,node1) = A(nb_nodes+testfunction_i,node1) + c1_hat;
+         A(nb_nodes+testfunction_i,node2) = A(nb_nodes+testfunction_i,node2) + c2_hat;
+         A(nb_nodes+testfunction_i,node3) = A(nb_nodes+testfunction_i,node3) + c3_hat;
+         b(nb_nodes+testfunction_i) = b(nd_nodes+testfunction_i) + int_6_2_lin;
      end
-     
+    
 end
 
-function [r] = calculate_r(ksi, y, X1, X2, X3)
+function [r] = r(ksi, y, X1, X2, X3)
     area = abs((X1(1)*X2(2) + X2(1)*X3(2) + X3(1)*X1(2) - X1(1)*X3(2) ...
                     - X2(1)*X1(2) - X3(1)*X2(2))/2);
     A = (X3(1) - X1(1))*(X2(2) - X1(2))/((X2(1)-X1(1))*(X3(2)-X1(2)));
@@ -144,7 +165,7 @@ function [r] = calculate_r(ksi, y, X1, X2, X3)
         (y*2*area-(X2(2)-X1(2))*X1(1))/(X2(1)-X3(1)))+X1(1)/(1-A);
 end 
 
-function [z] = calculate_z(ksi, y, X1, X2, X3)
+function [z] = z(ksi, y, X1, X2, X3)
     area = abs((X1(1)*X2(2) + X2(1)*X3(2) + X3(1)*X1(2) - X1(1)*X3(2) ...
                     - X2(1)*X1(2) - X3(1)*X2(2))/2);
     A = (X3(1) - X1(1))*(X2(2) - X1(2))/((X2(1)-X1(1))*(X3(2)-X1(2)));
