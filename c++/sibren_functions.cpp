724ba5911fb9646729b285f83c3b162c158f5acb
Sibrenlagauw <sibren@mlagauw.com>
2020-03-02 09:20:34 +0100

    first integral of (5) and (6) in c++

--- c++/sibren_functions.cpp
+++ c++/sibren_functions.cpp
@@ -6,59 +6,130 @@
 namespace pear{
 
 void createLinearSystem(mesh originalMesh, std::vector<double> K, std::vector<double> f, std::vector<float> setup){
+
   // Assert all elements in given K and f are zero.
 
   // nodes --> list met nodes van mesh (label, x, y)
   // elements --> list met elements van mesh (node1, node2, node3, boundary(boolean))
 
 
-  std::vector<int> currentElement(3);
+  std::vector<int> currentElement(4);
   boolean isBoundary;
   std::vector<float> P1(2);
   std::vector<float> P2(2);
   std::vector<float> P3(2);
+  int n1;
+  int n2;
+  int n3;
   double det_jac;
   double temp;
-  double result;
+  std::vector<double> commonPart(2);
+  double resultU;
+  double resultV;
+  int nbNodes = sqrt(K.size())/2; // TODO check if this works;
+
   for (int element = 0; element < elements.size(); element++){
     currentElement = elements[element];
     isBoundary = currentElement[3];
-    P1 = nodes[currentElement[1]];
-    P2 = nodes[currentElement[2]];
-    P3 = nodes[currentElement[3]];
+    n1 = currentElement[0];
+    n2 = currentElement[1];
+    n3 = currentElement[2];
+    P1 = nodes[n1];
+    P2 = nodes[n2];
+    P3 = nodes[n3];
 
-    det_jac = P1[2]*P2[1] - P1[1]*P2[2] + P1[1]*P3[2] - P1[2]*P3[1] - P2[1]*P3[2] + P2[2]*P3[1]; // Bewerking op single prec floats opslaan in double precision float mag???
-    temp = (P1[1] + P2[1] + P3[1])/(6*det_jac);
+    det_jac = P1[1]*P2[0] - P1[0]*P2[1] + P1[0]*P3[1] - P1[1]*P3[0] - P2[0]*P3[1] + P2[1]*P3[0]; // Bewerking op single prec floats opslaan in double precision float mag???
+    temp = (P1[0] + P2[0] + P3[0])/(6*det_jac);
 
+    // ---------------------------------------------------------------------------------
+    // This code snippet processes both the first integral of (5) and (6)
     // TODO: Integraal 1; eventueel deelfunctie maken hiervoor???
     // TODO: herschrijf deze assignments zodra we weten hoe sparse matrix voorgesteld wordt.
-      // Node 1
-    result = temp * (sigma_ur*pow(P3[1]-P2[1],2) + sigma_uz*pow(P3[2]-P2[2],2)) ;
-    K[currentElement[1],currentElement[1]] = K[currentElement[1],currentElement[1]] + result;
-    result = temp * (-sigma_ur*(P3[1]-P2[1])(P3[1]-P1[1]) + sigma_uz*(P2[2]-P3[2])*(P3[2]-P1[2])) ;
-    K[currentElement[1],currentElement[2]] = K[currentElement[1],currentElement[2]] + result;
-    result = temp * (sigma_ur*(P3[1]-P2[1])(P2[1]-P1[1]) - sigma_uz*(P2[2]-P3[2])*(P2[2]-P1[2])) ;
-    K[currentElement[1],currentElement[3]] = K[currentElement[1],currentElement[3]] + result;
+    // TODO: berekening commonPart vector kan geschreven worden in 1 lijn met vector bewerkingen?
+    //    door symmetrie van bewerkingen in r en z....
 
-      // Node 2
-    result = temp * (-sigma_ur*(P3[1]-P2[1])(P3[1]-P1[1]) + sigma_uz*(P2[2]-P3[2])*(P3[2]-P1[2])) ;
-    K[currentElement[2],currentElement[1]] = K[currentElement[2],currentElement[1]] + result;
-    result = temp * (sigma_ur*pow(P3[1]-P1[1],2) + sigma_uz*pow(P3[2]-P1[2],2)) ;
-    K[currentElement[2],currentElement[2]] = K[currentElement[2],currentElement[2]] + result;
-    result = temp * (-sigma_ur*(P3[1]-P1[1])(P2[1]-P1[1]) - sigma_uz*(P3[2]-P1[2])*(P2[2]-P1[2])) ;
-    K[currentElement[2],currentElement[3]] = K[currentElement[2],currentElement[3]] + result;
+
+    // Node 1
+    commonPart[0] = pow(P3[0]-P2[0],2);
+    commonPart[1] = pow(P3[1]-P2[1],2);
+    // commonPart is a vector which contains the part to multiply with sigma_r in [0] and
+    // the part with sigma_z in [1]...
+    applySigmaAndAddCommonPart(result, commonPart);
+    // result is vector with [result_u, result_v]
+    K[n1-1,n1-1] = K[n1-1,n1-1] + temp*result[0];
+    K[nbNodes+n1-1,nbNodes+n1-1] = K[nbNodes+n1-1,nbNodes+n1-1] + temp*result[1];
+
+
+    commonPart[0] = -(P3[0]-P2[0])*(P3[0]-P1[0]);
+    commonPart[1] = (P2[1]-P3[1])*(P3[1]-P1[1]);
+    applySigmaAndAddCommonPart(result, commonPart);
+    K[n1-1,n2-1] = K[n1-1,n2-1] + temp*result[0];
+    K[nbNodes+n1-1,nbNodes+n2-1] = K[nbNodes+n1-1,nbNodes+n2-1] + temp*result[1];
+
+    commonPart[0] = (P3[0]-P2[0])*(P2[0]-P1[0]);
+    commonPart[1] = -(P2[1]-P3[1])*(P2[1]-P1[1]);
+    applySigmaAndAddCommonPart(result, commonPart);
+    K[n1-1,n3-1] = K[n1-1,n3-1] + temp*result[0];
+    K[nbNodes+n1-1,nbNodes+n3-1] = K[nbNodes+n1-1,nbNodes+n3-1] + temp*result[1];
+
+
+
+    // Node 2
+    commonPart[0] = -(P3[0]-P2[0])*(P3[0]-P1[0]);
+    commonPart[1] = (P2[1]-P3[1])*(P3[1]-P1[1]);
+    applySigmaAndAddCommonPart(result, commonPart);
+    K[n2-1,n1-1] = K[n2-1,n1-1] + temp*result[0];
+    K[nbNodes+n2-1,nbNodes+n1-1] = K[nbNodes+n2-1,nbNodes+n1-1] + temp*result[1];
+
+    commonPart[0] = pow(P3[0]-P1[0], 2);
+    commonPart[1] = pow(P3[1]-P1[1], 2);
+    applySigmaAndAddCommonPart(result, commonPart);
+    K[n2-1,n2-1] = K[n2-1,n2-1] + temp*result[0];
+    K[nbNodes+n2-1,nbNodes+n2-1] = K[nbNodes+n2-1,nbNodes+n2-1] + temp*result[1];
+
+    commonPart[0] = -(P3[0]-P1[0])*(P2[0]-P1[0]);
+    commonPart[1] = -(P3[1]-P1[1])*(P2[1]-P1[1]);
+    applySigmaAndAddCommonPart(result, commonPart);
+    K[n2-1,n3-1] = K[n2-1,n3-1] + temp*result[0];
+    K[nbNodes+n2-1,nbNodes+n3-1] = K[nbNodes+n2-1,nbNodes+n3-1] + temp*result[1];
+
 
       // Node 3
-    result = temp * (sigma_ur*(P3[1]-P2[1])(P2[1]-P1[1]) - sigma_uz*(P2[2]-P3[2])*(P2[2]-P1[2])) ;
-    K[currentElement[2],currentElement[1]] = K[currentElement[2],currentElement[1]] + result;
-    result = temp *  (-sigma_ur*(P3[1]-P1[1])(P2[1]-P1[1]) - sigma_uz*(P3[2]-P1[2])*(P2[2]-P1[2])) ;
-    K[currentElement[2],currentElement[2]] = K[currentElement[2],currentElement[2]] + result;
-    result = temp * (sigma_ur*pow(P2[1]-P1[1],2) + sigma_uz*pow(P2[2]-P1[2],2)) ;
-    K[currentElement[2],currentElement[3]] = K[currentElement[2],currentElement[3]] + result;
+    commonPart[0] = (P3[0]-P2[0])*(P2[0]-P1[0]);
+    commonPart[1] = -(P2[1]-P3[1])*(P2[1]-P1[1]);
+    applySigmaAndAddCommonPart(result, commonPart);
+    K[n3-1,n1-1] = K[n3-1,n1-1] + temp*result[0];
+    K[nbNodes+n3-1,nbNodes+n1-1] = K[nbNodes+n3-1,nbNodes+n1-1] + temp*result[1];
 
+    commonPart[0] = -(P3[0]-P1[0])*(P2[0]-P1[0]);
+    commonPart[1] = -(P3[1]-P1[1])*(P2[1]-P1[1]);
+    applySigmaAndAddCommonPart(result, commonPart);
+    K[n3-1,n2-1] = K[n3-1,n2-1] + temp*result[0];
+    K[nbNodes+n3-1,nbNodes+n2-1] = K[nbNodes+n3-1,nbNodes+n2-1] + temp*result[1];
 
+    commonPart[0] = pow(P2[0]-P1[0], 2);
+    commonPart[1] = pow(P2[1]-P1[1], 2);
+    applySigmaAndAddCommonPart(result, commonPart);
+    K[n3-1,n3-1] = K[n3-1,n3-1] + temp*result[0];
+    K[nbNodes+n3-1,nbNodes+n3-1] = K[nbNodes+n3-1,nbNodes+n3-1] + temp*result[1];
+
+    // -----------------------------------------------------------------------------------------
 
   }
+
+
+}
+
+void applySigmaAndAddCommonPart(std::vector<double> result(2), std::vector<double> commonPart(2)){
+  // TODO: volgens wiskundige afleiding moet sigma_uz maal r gedeelte en andersom, dit is niet hoe wij het in MATLAB doen!!!
+  double sigma_ur = ;
+  double sigma_uz = ;
+  double sigma_vr = ;
+  double sigma_vz = ;
+  result[0] = sigma_ur*commonPart[0] + sigma_uz*commonPart[1];
+  result[1] = sigma_vr*commonPart[0] + sigma_vz*commonPart[1];
+
+  return;
 }
 
 
