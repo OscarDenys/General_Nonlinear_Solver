1a814bed2c37ba668865e690180180a69936a6df
Oscar <oscar.denys@student.kuleuven.be>
2020-04-15 18:42:24 +0200

    ceres installation

---
+++ c++/ceres-bin/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/Git/PME/c++/ceres-solver-1.14.0
+# Build directory: /home/parallels/Git/PME/c++/ceres-bin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("internal/ceres")
+subdirs("examples")
---
+++ c++/ceres-bin/examples/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/parallels/Git/PME/c++/ceres-solver-1.14.0/examples
+# Build directory: /home/parallels/Git/PME/c++/ceres-bin/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("sampled_function")
+subdirs("slam")
