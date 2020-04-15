088b9c00e7d9447635a2cf439f7768f3d7ce62ef
Oscar <oscar.denys@student.kuleuven.be>
2020-04-15 09:43:55 +0200

    output change

--- c++/LM.cpp
+++ c++/LM.cpp
@@ -77,10 +77,10 @@ void finite_difference_jacob(arrayxd &f0, spmat & J, void (*Ffun)(spmat &, array
         std::mesh &myMesh, spmat &Kstelsel, arrayxd &fstelsel){
 
     int Nx = x0.size();
-    (*Ffun)(Kstelsel, fstelsel, x0, f0, myMesh);
+    (*Ffun)(Kstelsel, fstelsel, x0, f0, myMesh); // F0 = F(x0)
 
     // perform finite difference jacobian evaluation
-    double h = 1e-6; // stepsize for first order approximation
+    double h = 1e-6; // stepsize for first order approximation // original 1e-6
     std::vector<Trip> tripletList; //triplets.reserve(estimation_of_entries); //--> how many nonzero elements in J?
 
     arrayxd x = x0;
@@ -135,7 +135,7 @@ OUTPUT
 void minimize_lm(std::mesh &myMesh, arrayxd & x, void (*Ffun)(spmat&, arrayxd&,arrayxd&, arrayxd&, std::mesh&), arrayxd &x0, spmat &Kstelsel, arrayxd &fstelsel){
 
     // convergence tolerance
-    double grad_tol = 1e-8;
+    double grad_tol = 1e-8; // original 1e-4 denk ik
     int max_iters = 200;
   
     arrayxd F(x0.size());
@@ -182,7 +182,7 @@ void minimize_lm(std::mesh &myMesh, arrayxd & x, void (*Ffun)(spmat&, arrayxd&,a
         if (inf_norm_grad < grad_tol ){
             //x_iter = x_iter.block(0,0,Nx,k); // this gave segmentation fault
             //grad_iter = grad_iter.head(k);
-            //std::cout<< "solution: "<< x << std::endl;
+            //std::cout<< "solution : "<< x  << std::endl;
             return;
         }
 
@@ -210,13 +210,13 @@ void minimize_lm(std::mesh &myMesh, arrayxd & x, void (*Ffun)(spmat&, arrayxd&,a
         double Jpk = grad.dot(pk); 
 
 
-        if (k > 1) {
+        //if (k > 1) {
             arrayxd xold = x;
-            double fxold = f(Kstelsel,fstelsel, Ffun, x, myMesh);
-            line_search(x, f, Ffun, x, Jpk, pk, gamma, beta, myMesh,  Kstelsel, fstelsel);
+            double fxold = f(Kstelsel,fstelsel, Ffun, xold, myMesh);
+            line_search(x, f, Ffun, xold, Jpk, pk, gamma, beta, myMesh,  Kstelsel, fstelsel);
             double fxnew = f(Kstelsel,fstelsel, Ffun, x, myMesh);
-
-            if (fxnew < fxold){ // step accepted and lambda decreased to make the next step more like fast Gauss Newton step
+        if (k > 1) {
+            if (fxnew < fxold){ // step accepted and lambda decreased to make the next step more like the Gauss Newton step (faster than gradient descent)
                 lambda = lambda * lamdascaling;
             }
             else{ // step declined and lambda increased to make the next step more like the stable gradient descent step
@@ -234,6 +234,9 @@ void minimize_lm(std::mesh &myMesh, arrayxd & x, void (*Ffun)(spmat&, arrayxd&,a
         std::cout<< "iteration: "<< k << "  inf_norm_grad = "<< inf_norm_grad << " f(x) = " << f(Kstelsel,fstelsel, Ffun, x, myMesh) << std::endl;
 
         if (count_fxsmall ==2){
+            //std::cout<< " f(x) : "<< f(Kstelsel,fstelsel, Ffun, x, myMesh)  << std::endl; // DIT GEEFT EEN ERROR
+            //std::cout<< " f(x0) : "<< f(Kstelsel,fstelsel, Ffun, x0, myMesh)  << std::endl;
+            /* Assertion `row>=0 && row<rows() && col>=0 && col<cols()' failed komt uit Eigen/src/SparseCore/SparseMatrix.h:208: */
             return;
         }
     }
