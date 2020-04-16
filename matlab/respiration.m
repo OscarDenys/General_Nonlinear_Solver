d848a404b8aee16e33d7b5fda48fc96df0f33cb1
Sem Viroux <sem.viroux@hotmail.com>
2020-04-16 18:14:02 +0200

    MMS

--- matlab/respiration.m
+++ matlab/respiration.m
@@ -7,8 +7,9 @@ function [R_u,R_v] = respiration(index1,index2,prev_sol, vars)
    
     C_u = (prev_sol(index1) + prev_sol(index2)) / 2;
     C_v = (prev_sol(M+index1) + prev_sol(M+index2)) / 2;
- 
     
+%     C_u = 8.500176555844678;
+%     C_v = 0.016346493376624;
     
     R_u = vars.V_mu * C_u / ((vars.K_mu+C_u)*(1+ C_v/vars.K_mv));
     R_v = vars.r_q * R_u + vars.V_mfv / (1+ C_u/vars.K_mfu);
